d37ae2c0dcec9dc78701f84e0d528c753d58c155
Dusty Little <dlittle@toyatech.net>
2014-01-17 12:39:21 -0800

    fix byte shifting

--- binarius.js
+++ binarius.js
@@ -40,13 +40,25 @@ Parser.prototype.structure = {
   int32be: function () { return this._data.readInt32BE(this._offset++); },
   doublebe: function () { return this._data.readDoubleBE(this._offset++); },
   floatbe: function () { return this._data.readFloatBE(this._offset++); },
+  bit: function (length) {
+    length = toInt.call(this, length);
+    var result = 
+      this._data.readUInt8(this._offset) >> (8 - this._bitShift - length)
+      & ((1 << length) - 1);
+    this._bitShift += length;
+    if (this._bitShift === 8) {
+      this._offset++;
+      this._bitShift = 0;
+    }
+    return result;
+  }, 
   char: function () { return this._data.toString(this._offset++, 1); },
   string: function (length) { 
     return this._data.toString(this._offset++, length);
   },
   array: function (type, length) {
     length = toInt.call(this, length);
-    var result = [];
+    var results = [];
     for (var i = 0; i < length; ++i) {
       results.push(this.parse(type));
     }
@@ -82,14 +94,7 @@ Parser.prototype.skip = Parser.prototype.structure.skip;
 
 Parser.prototype.parse = function(structure) {
   if (typeof structure === 'number') {
-    var fieldValue = 0,
-      bitSize = structure;
-    
-    if (bitSize > 0) {
-      fieldValue = ((this._data.readUInt8(this._offset) >>> (8 - (this._bitShift + bitSize))) & ~(-1 << bitSize)) | (fieldValue << bitSize);
-      this._bitShift += bitSize - 8;
-    }
-    return fieldValue;
+    return this.structure.bit.call(this, structure);
   }
 
   if (structure instanceof Function) {
@@ -128,3 +133,119 @@ Parser.prototype.parse = function(structure) {
 };
 
 exports.Parser = Parser;
+
+function Serializer(data, structure) {
+  this._offset = 0;
+  this._bitShift = 0;
+  this._data = data;
+  this.structure =
+    inherit(Serializer.prototype.structure, structure);
+}
+
+Serializer.prototype.structure = {
+  uint8: function () { return this._data.writeUInt8(this._offset++); },
+  uint16: function () { return this._data.writeUInt16LE(this._offset++); },
+  uint32: function () { return this._data.writeUInt32LE(this._offset++); },
+  int8: function () { return this._data.writeInt8(this._offset++); },
+  int16: function () { return this._data.writeInt16LE(this._offset++); },
+  int32: function () { return this._data.writeInt32LE(this._offset++); },
+  double: function () { return this._data.writeDoubleLE(this._offset++); },
+  float: function () { return this._data.writeFloatLE(this._offset++); },
+  uint16be: function () { return this._data.writeUInt16BE(this._offset++); },
+  uint32be: function () { return this._data.writeUInt32BE(this._offset++); },
+  int16be: function () { return this._data.writeInt16BE(this._offset++); },
+  int32be: function () { return this._data.writeInt32BE(this._offset++); },
+  doublebe: function () { return this._data.writeDoubleBE(this._offset++); },
+  floatbe: function () { return this._data.writeFloatBE(this._offset++); },
+  bit: function (length) {
+    length = toInt.call(this, length);
+    var result =
+      this._data.readUInt8(this._offset) >> (8 - this._bitShift - length)
+      & ((1 << length) - 1);
+    this._bitShift += length;
+    if (this._bitShift === 8) {
+      this._offset++;
+      this._bitShift = 0;
+    }
+    return result;
+  },
+  char: function () { return this._data.toString(this._offset++, 1); },
+  string: function (length) {
+    return this._data.toString(this._offset++, length);
+  },
+  array: function (type, length) {
+    length = toInt.call(this, length);
+    var results = [];
+    for (var i = 0; i < length; ++i) {
+      results.push(this.parse(type));
+    }
+    return results;
+  },
+  seek: function (position, block) {
+    position = toInt.call(this, position);
+    if (block instanceof Function) {
+      var old_position = this._offset;
+      this._offset = position;
+      var result = block.call(this);
+      this._offset = old_position;
+      return result;
+    } else {
+      return this._offset = position;
+    }
+  },
+  tell: function () { return this._offset; },
+  skip: function (offset) {
+    offset = toInt.call(this, offset);
+    return this._offset += offset;
+  },
+  if: function (predicate) {
+    if (predicate instanceof Function ? predicate.call(this) : predicate) {
+      return this.parse.apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+  }
+};
+
+Serializer.prototype.seek = Serializer.prototype.structure.seek;
+Serializer.prototype.tell = Serializer.prototype.structure.tell;
+Serializer.prototype.skip = Serializer.prototype.structure.skip;
+
+Serializer.prototype.serialize = function(structure) {
+  if (typeof structure === 'number') {
+    return this.structure.bit.call(this, structure);
+  }
+
+  if (structure instanceof Function) {
+    return structure.apply(this, Array.prototype.slice.call(arguments, 1));
+  }
+
+  if (typeof structure === 'string') {
+    structure = Array.prototype.slice.call(arguments);
+  }
+
+  if (structure instanceof Array) {
+    var key = structure[0];
+    if (!(key in this.structure)) {
+      throw new Error('Missing structure for `' + key + '`');
+    }
+    return this.parse.apply(
+      this, [this.structure[key]].concat(structure.slice(1)));
+  }
+
+  if (typeof structure === 'object') {
+    var output = {},
+      current = this.current;
+    this.current = output;
+
+    for (var key in structure) {
+      var value = this.parse(structure[key]);
+      if (value !== undefined) {
+        output[key] = value;
+      }
+    }
+    this.current = current;
+    return output;
+  }
+
+  throw new Error('Unknown structure type`' + structure + '`');
+};
+
