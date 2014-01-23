0ee39a9bd2e6691ec1f2df28d52ef6b5d648660e
noisypebble <noisypebble@toyatech.net>
2014-01-22 21:59:11 -0600

    fixes after refactor

--- test/test-decoder.js
+++ test/test-decoder.js
@@ -4,15 +4,31 @@ var assert = require('assert')
 var PRIMATIVE_TYPES = require('../lib/types').PRIMATIVE_TYPES;
 
 describe('Decoder', function() {
-  var decoder = new Decoder();
+  var decoder = new Decoder({data: 'uint8'});
   Object.keys(PRIMATIVE_TYPES)
     .forEach(function(type) {
-      describe('#prototype.' + type.toLowerCase(), function() {
+      describe('#_struct.' + type.toLowerCase() + '()', function() {
         it('should be an instance of Function', function() {
           assert.equal(true, 
-            decoder[type.toLowerCase()] instanceof Function);
+            decoder._struct[type.toLowerCase()] instanceof Function);
         });
       });
     });
+  Object.keys(PRIMATIVE_TYPES)
+    .forEach(function(type) {
+      var len = PRIMATIVE_TYPES[type];
+      var buf = new Buffer(len);
+      for (var i = 0;i < len;i++) {
+        buf[i] = i;
+      }
+      var val = buf['read' + type](0);
+      describe('#decode() a ' + type + ' type', function() {
+        it('should return an object with data key equal to ' + val, function() {
+          decoder = new Decoder({data: type.toLowerCase()});
+          var obj = decoder.decode(buf, 'data');
+          assert.equal(val, obj);
+        });
+      });
+    }); 
 });
         
